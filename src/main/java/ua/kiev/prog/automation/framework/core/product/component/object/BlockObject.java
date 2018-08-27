063d0a91218b045e6aa825625e0369d29fa35974
Iree Kryvenko <36423906+ivkua@users.noreply.github.com>
2018-08-28 01:10:32 +0300

    Update BlockObject.java

--- src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/BlockObject.java
+++ src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/BlockObject.java
@@ -1,14 +1,4 @@
 package ua.kiev.prog.automation.framework.core.product.component.object;
 
-/**
- * ////////////////////////////////////////////////////////// *
- * Automation Framework                                       *
- * Automation Course for https://prog.kiev.ua/                *
- * ---------------------------------------------------------- *
- * Created by Yurii Voronenko                                 *
- * Email: yurii.voronenko@gmail.com                           *
- * ////////////////////////////////////////////////////////// *
- */
-abstract public class BlockObject extends PageObject
-{
+abstract public class BlockObject extends PageObject{
 }
