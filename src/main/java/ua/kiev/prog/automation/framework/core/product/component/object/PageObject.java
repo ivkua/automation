432cc311403cd8d9b2ddd7de5774fc3882d4af19
Iree Kryvenko <36423906+ivkua@users.noreply.github.com>
2018-08-28 01:13:03 +0300

    Update PageObject.java

--- src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/PageObject.java
+++ src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/PageObject.java
@@ -6,92 +6,41 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ua.kiev.prog.automation.framework.core.product.Component;
 
-/**
- * ////////////////////////////////////////////////////////// *
- * Automation Framework                                       *
- * Automation Course for https://prog.kiev.ua/                *
- * ---------------------------------------------------------- *
- * Created by Yurii Voronenko                                 *
- * Email: yurii.voronenko@gmail.com                           *
- * ////////////////////////////////////////////////////////// *
- */
-abstract public class PageObject<T extends PageObject>
-{
-    /**
-     * Компонент объекта страницы
-     */
+abstract public class PageObject<T extends PageObject>{
     private Component _component;
 
-    /**
-     * Конструктор объекта
-     */
-    protected PageObject ()
-    {
-        // Считываем результат метода componentClass() (абстрактный, определён в наследнике) в переменную
+    protected PageObject () {
         Class<? extends Component> componentClass = this.componentClass();
-        // Если метод вернул null, то бросаем исключение с описанием ошибки
         if (componentClass == null)
             throw new RuntimeException("Method componentClass returns null. Class: " + this.getClass().getName());
-
-        // Получаем объект-одиночку(Singleton) из списка, в статических свойствах класса Component по имени класса
+        
         this._component = Component.getSingleton(componentClass);
-
-        // Денлаем искуственную задержку на 1 сек для наглядности
+        
         try {
             Thread.sleep(1000);
-        } catch (Exception e) { /* Ignore */ }
-        // Ждем подтверждения загрузки страниці
+        } catch (Exception e) {}
         this.waitReadyLocator();
     }
 
-    /**
-     * Этот метод возвращает компонент объекта страницы
-     *
-     * @return Component - page component
-     */
-    protected Component component()
-    {
+    protected Component component() {
         return this._component;
     }
 
-    /**
-     * Этот метод должен возвращать класс компонета для объекта страницы
-     * Определяет к какому компоненту привязан этот объект страницы
-     *
-     * @return Class
-     */
     abstract protected Class<? extends Component> componentClass();
 
-    /**
-     * Єтот метод должен возвращать локатор готовности страницы
-     *
-     * @return By - locator
-     */
     abstract protected By readyLocator ();
 
-    /**
-     * Этот метод ждет, пока єлемент (по локатору готовности) не будет виден
-     */
     private void waitReadyLocator ()
     {
         try {
-            // Для читаемости, перенесем данные в переменные
-            RemoteWebDriver driver  = this.component().session().driver();  // Получаем драйвер сессии компонента
-            By readyLocator         = this.readyLocator();                  // Получаем локатор готовности объекта страницы
-            // Создаем объект, который будет работать через наш драйвер с таймаутом 30 сек
+            RemoteWebDriver driver  = this.component().session().driver(); 
+            By readyLocator         = this.readyLocator();                  
             WebDriverWait wait      = new WebDriverWait(driver, 30);
-            // Запускаем ожидание
             wait.until(ExpectedConditions.visibilityOfElementLocated(readyLocator));
-        } catch (Exception e) { /* Ignore */ }
+        } catch (Exception e) {}
     }
 
-    /**
-     * Этот метод возвращает статус удачной загрузки страницы
-     *
-     * @return boolean - true если страница корректна и подтверждена локатором готовности
-     */
-    final public boolean success ()
-    {
+    final public boolean success (){
         boolean result;
         try {
             result = this.component().session().driver().findElement(this.readyLocator()).isDisplayed();
@@ -105,5 +54,4 @@ abstract public class PageObject<T extends PageObject>
     {
         return this.component().session().driver();
     }
-
 }
